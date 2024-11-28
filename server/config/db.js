9216bb1ffda5edd6b204bdda2ca4c13bf6373bca
Ajmal Faris <ajmalfaris.me@gmail.com>
2024-11-29 04:03:10 +0530

    Configure MongoDB connection using environment variable
    - Updated the MongoDB connection in `db.js` to use the `MONGO_URL` environment variable for secure and flexible database connection management.
    - The connection string is stored in the `.env` file, allowing for easy modification without changing the codebase.
    - This improves security by avoiding hardcoding sensitive database credentials in the source code.

--- server/config/db.js
+++ server/config/db.js
@@ -3,15 +3,16 @@ const mongoose = require("mongoose");
 
 // Function to establish a connection to the MongoDB database
 const connectDB = async () => {
-    try {
-        // Attempting to connect to MongoDB using a connection string (replace with your own)
-        await mongoose.connect("mongodb+srv://ajmalfaris-mern:OYEXIodpRwfCBQaG@cluster0.gedfg.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0");
-        // If successful, log a success message
-        console.log("DB connected successfully");
-    } catch (err) {
-        // If an error occurs, log the error message
-        console.log(error);
-    }
+  try {
+    // Attempting to connect to MongoDB using a connection string from the environment variable
+    await mongoose.connect(process.env.MONGO_URL);
+
+    // If successful, log a success message
+    console.log("DB connected successfully");
+  } catch (err) {
+    // If an error occurs, log the error message
+    console.log(error);
+  }
 };
 
-module.exports = connectDB; // export the connectDB async function to import on index.js file 
+module.exports = connectDB; // export the connectDB async function to import on index.js file
