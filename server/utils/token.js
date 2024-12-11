5f3846be683a559cdb5d1196a5565a3c1babe315
Ajmal Faris <ajmalfaris.me@gmail.com>
2024-12-12 03:33:48 +0530

    created JWT token generation function

---
+++ server/utils/token.js
@@ -0,0 +1,24 @@
+// Import the jsonwebtoken library
+const jwt = require("jsonwebtoken");
+
+// Function to generate a JWT token
+// Accepts user object and role as parameters
+const generaeteToken = (user, role) => {
+    try {
+        // Generate a JWT token with user ID and role
+        const token = jwt.sign(
+            { id: user._id, role: role }, // Payload data
+            process.env.JWT_SECRET          // Secret key from environment variables
+        );
+        
+        // Return the generated token
+        return token;
+
+    } catch (error) {
+        // Handle any errors that occur during token generation
+        res.status(500).json({ err: error.message || "Internal Server Error" });
+    }
+}
+
+// Export the token generation function for external use
+module.exports = generaeteToken;
